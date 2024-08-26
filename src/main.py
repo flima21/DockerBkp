568b77885d5e05bdd3b4572cffeda5c4d45a578f
Felipe Lima <felipe.pedroso2108@gmail.com>
2024-08-26 12:06:19 -0300

    remove: containers from bkp

--- src/main.py
+++ src/main.py
@@ -10,22 +10,22 @@ DEBUG = False
 # instance docker
 instance = docker.from_env()
 
-containers = instance.containers.list() # all containers
+# containers = instance.containers.list() # all containers
 volumes = instance.volumes.list() # all volumes
 
-for c in containers:
-    if DEBUG == False:
-        c.commit(repository=f"{c.name}_bkp_{TODAY}",tag=TODAY)
+# for c in containers:
+#     if DEBUG == False:
+#         c.commit(repository=f"{c.name}_bkp_{TODAY}",tag=TODAY)
 
-    else: 
-        print(c.name)
+#     else: 
+#         print(c.name)
 
 for v in volumes:
     if DEBUG == False:
-        make_archive(f"VolumesBkp/Volume{v.name}{TODAY}",'zip',root_dir=v.attrs.get('Mountpoint'))            
+        make_archive(f"VolumesBkp/Volume{v.name.upper()}{TODAY}",'zip',root_dir=v.attrs.get('Mountpoint'))            
     else:
         print(v.name)
         print(v.attrs)
 instance.close()
 
-print(f"BKP CREATED IN {os.getcwd()}")
+print(f"BKP CREATED IN VolumesBKP{os.getcwd()}")
